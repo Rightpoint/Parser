dc508943b9e9b7e139ba5bcb0933ac8a2558d08c
Andrew Grosner <andrew.grosner@raizlabs.com>
2014-11-05 12:44:14 -0500

    adding more documentation

--- library/src/main/java/com/raizlabs/android/parser/ObjectParser.java
+++ library/src/main/java/com/raizlabs/android/parser/ObjectParser.java
@@ -5,11 +5,23 @@ package com.raizlabs.android.parser;
  * Contributors: { }
  * Description: Internal usage that will generate how to parse an object
  */
-public interface ObjectParser<ParseableClass>  {
+public interface ObjectParser<ParseableClass> {
 
+    /**
+     * Returns a new instance of the {@link ParseableClass} without using reflection.
+     *
+     * @return
+     */
     public ParseableClass getInstance();
 
-    public void parse(ParseableClass parsible, Object instance, Parser parser);
+    /**
+     * Will set all of the fields for the {@link ParseableClass} that define a {@link com.raizlabs.android.parser.core.Key} attribute.
+     *
+     * @param parsible     The instance that we are parsing
+     * @param dataInstance The singular data type that we're parsing. E.g. {@link org.json.JSONObject}
+     * @param parser       The parser we're using to parse the dataInstance into the parsible
+     */
+    public void parse(ParseableClass parsible, Object dataInstance, Parser parser);
 
 
 }

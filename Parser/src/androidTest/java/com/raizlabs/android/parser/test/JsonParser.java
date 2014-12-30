e869ed3c095ad911e54a77237bf9367686178d1a
Andrew Grosner <andrew.grosner@raizlabs.com>
2014-12-30 17:18:34 -0500

    made json parser included in library and all you need to do is extend and add annnotation to class now

--- Parser/src/androidTest/java/com/raizlabs/android/parser/test/JsonParser.java
+++ Parser/src/androidTest/java/com/raizlabs/android/parser/test/JsonParser.java
@@ -1,37 +1,10 @@
 package com.raizlabs.android.parser.test;
 
-
-import com.raizlabs.android.parser.BaseParser;
-import com.raizlabs.android.parser.JSON;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.util.List;
+import com.raizlabs.android.parser.core.ParseInterface;
 
 /**
- * Author: andrewgrosner
- * Description: Example JSON parser
+ * Description: Example of easy extension
  */
-@com.raizlabs.android.parser.core.ParseInterface
-public class JsonParser extends BaseParser<JSONObject, JSONArray> {
-    @Override
-    public Object getValue(JSONObject object, String key, Object defValue, boolean required) {
-        return JSON.getValue(object, key, defValue, required);
-    }
-
-    @Override
-    public <ReturnType> ReturnType getObject(Class<ReturnType> returnType, JSONArray jsonArray, int index) {
-        return JSON.get(returnType, jsonArray, index);
-    }
-
-    @Override
-    public List<String> keys(JSONObject jsonObject) {
-        return JSON.keys(jsonObject);
-    }
-
-    @Override
-    public int count(JSONArray jsonArray) {
-        return JSON.count(jsonArray);
-    }
+@ParseInterface
+public class JsonParser extends com.raizlabs.android.parser.JsonParser {
 }

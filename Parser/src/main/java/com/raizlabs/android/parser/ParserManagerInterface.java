f76c1a81cda3b89b9c53eed997273a077d284ed0
Eric Slosser <eric.slosser@raizlabs.com>
2016-05-19 16:04:06 -0400

    delete all but README from master

--- Parser/src/main/java/com/raizlabs/android/parser/ParserManagerInterface.java
+++
@@ -1,8 +0,0 @@
-package com.raizlabs.android.parser;
-
-/**
- * Author: andrewgrosner
- * Description: Internal usage for creating the ParserManager that puts the data into the {@link com.raizlabs.android.parser.ParserHolder}
- */
-interface ParserManagerInterface {
-}
